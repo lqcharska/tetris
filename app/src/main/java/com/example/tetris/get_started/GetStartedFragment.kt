95f49ec1af79f055106769143027794ffae33314
lullusiek <lullusiek@gmail.com>
2021-10-09 19:48:26 +0200

    init commit

---
+++ app/src/main/java/com/example/tetris/get_started/GetStartedFragment.kt
@@ -0,0 +1,11 @@
+package com.example.tetris.get_started
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GetStartedFragment : Fragment() {
+
+}
