b6ec46b693129c4ee26b983eda5693a0cbf673d3
lullusiek <lullusiek@gmail.com>
2021-10-09 22:00:29 +0200

    data binding works!

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.get_started_fragment)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/tetris/get_started/GetStartedFragment.kt
+++ app/src/main/java/com/example/tetris/get_started/GetStartedFragment.kt
@@ -1,11 +1,29 @@
 package com.example.tetris.get_started
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.tetris.R
+import com.example.tetris.databinding.GetStartedFragmentBinding
 
 class GetStartedFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        val binding: GetStartedFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.get_started_fragment, container, false
+        )
+
+        binding.startButton.setOnClickListener {
+            Log.d("dupa", "dupa")
+        }
+        return binding.root
+    }
+
 
 }
