7c2244f07bf88e1cdf4fc8ba38734e1db0113eed
lullusiek <lullusiek@gmail.com>
2021-10-18 23:39:00 +0200

    Gameboard made

--- app/src/main/java/com/example/tetris/game/GameFragment.kt
+++ app/src/main/java/com/example/tetris/game/GameFragment.kt
@@ -7,9 +7,13 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GestureDetectorCompat
+import androidx.core.view.MotionEventCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.tetris.R
@@ -17,24 +21,21 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.example.tetris.databinding.GameFragmentBinding
+import com.example.tetris.game.GameViewModel
+import android.content.Context as Context
 
 class GameFragment : Fragment() {
 
 
     private lateinit var binding: GameFragmentBinding
+    lateinit var gameModel : GameViewModel
+    private lateinit var moveDetector: GestureDetectorCompat
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        val bitmap: Bitmap = Bitmap.createBitmap(700,1000, Bitmap.Config.ARGB_8888)
-        val canvas: Canvas = Canvas(bitmap)
-        val paint: Paint = Paint()
-        paint.setColor(Color.MAGENTA)
-        canvas.drawRect(10F,20F,30F,40F,paint)
-
-
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
@@ -43,13 +44,10 @@ class GameFragment : Fragment() {
             container,
             false
         )
-
         binding.endButton.setOnClickListener {}
         binding.pauseButton.setOnClickListener {}
-
-        binding.imageView2.background = BitmapDrawable(resources, bitmap)
+        gameModel = GameViewModel(binding.gameSpaceView, this)
         return binding.root
-
     }
 
 }
--- app/src/main/java/com/example/tetris/game/GameViewModel.kt
+++ app/src/main/java/com/example/tetris/game/GameViewModel.kt
@@ -1,9 +1,66 @@
 package com.example.tetris.game
 
-import android.content.Context
-import android.util.Log
-import androidx.lifecycle.ViewModel
 
-class GameViewModel() : ViewModel(){
+import android.app.Activity
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
+import android.util.Log
+import android.widget.ImageView
+import androidx.lifecycle.ViewModel
+import com.example.tetris.get_started.GameFragment
+
+class GameViewModel(imageView: ImageView, context: GameFragment) : ViewModel(){
+
+    lateinit var bitmap : Bitmap
+    lateinit var canvas: Canvas
+    lateinit var paint: Paint
+    var gameView = imageView
+    var gameContext = context
+    var gameBoard = arrayOf(
+        arrayOf(0,0,0,0,0,0,0,0,0,0),
+        arrayOf(0,0,0,0,0,0,0,1,0,0),
+        arrayOf(0,0,1,0,0,0,0,0,0,0),
+        arrayOf(0,0,0,0,1,0,0,0,0,0),
+        arrayOf(0,0,0,0,0,0,0,0,0,0),
+        arrayOf(0,0,1,0,0,0,0,0,0,1),
+        arrayOf(0,0,0,1,0,0,0,0,0,0),
+        arrayOf(0,0,0,0,1,0,0,0,0,0),
+        arrayOf(0,0,0,0,0,1,0,0,0,0),
+    )
+    init {
+        bitmap = Bitmap.createBitmap(700, 1000, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(bitmap)
+        paint = Paint()
+        paint.setColor(Color.MAGENTA)
+//        canvas.drawRect(10F, 20F, 30F, 40F, paint)
+        drawGameBoard()
+//        gameBoard[0][5] = 1
+
+    }
+
+    private fun drawGameBoard(){
+        var counter_i : Int = 0
+        var counter_j : Int = 0
+        var multipler : Float = 70.0F
+        for(i in gameBoard){
+            counter_j = 0
+            for(j in i){
+                if (j == 1){
+                    canvas.drawRect(counter_j.toFloat()*multipler,
+                        counter_i.toFloat()*multipler,
+                        (counter_j+1).toFloat()*multipler, (counter_i+1).toFloat()*multipler, paint)
+                }
+                counter_j += 1
+            }
+            counter_i += 1
+        }
+        gameView.background = BitmapDrawable(gameContext.resources, bitmap)
+    }
+
 
 }
