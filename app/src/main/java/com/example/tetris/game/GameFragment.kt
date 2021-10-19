cee63fa714ad511bce42b27b4ce7266ab727f5fc
lullusiek <lullusiek@gmail.com>
2021-10-19 22:34:34 +0200

    Application changed to normal style of programming

--- app/src/main/java/com/example/tetris/game/GameFragment.kt
+++ app/src/main/java/com/example/tetris/game/GameFragment.kt
@@ -1,6 +1,11 @@
 package com.example.tetris.get_started
 
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
@@ -10,11 +15,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Chronometer
 import android.widget.Chronometer.OnChronometerTickListener
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tetris.R
 import com.example.tetris.databinding.GameFragmentBinding
@@ -26,8 +30,18 @@ class GameFragment : Fragment() {
     private lateinit var binding: GameFragmentBinding
     private lateinit var gameModel : GameViewModel
     private lateinit var chronometer: Chronometer
+
+
+    private lateinit var bitmap : Bitmap
+    private lateinit var canvas : Canvas
+    private lateinit var paint : Paint
+
+
+    private var gameViewWidth : Int = 0
+    private var gameViewHeight : Int = 0
     private var running : Boolean = false
     private var pauseOffset : Int = 0
+
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
@@ -42,25 +56,62 @@ class GameFragment : Fragment() {
             container,
             false
         )
+        gameViewWidth = binding.gameSpaceView.layoutParams.width
+        gameViewHeight = binding.gameSpaceView.layoutParams.height
 
-//        gameModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        // Creating space for game
+        bitmap = Bitmap.createBitmap(gameViewWidth, gameViewHeight, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(bitmap)
+        paint = Paint()
+        paint.color = Color.MAGENTA
+        paint.style = Paint.Style.STROKE
 
+        gameModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        // Observe
+        gameModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        // Observe
+        gameModel.gameBoard.observe(viewLifecycleOwner, Observer { newGameBoard ->
+            var counterI : Int = 0
+            var counterJ : Int = 0
+            val multiplierI : Float = (gameViewWidth / 10).toFloat()
+            val multiplierJ : Float = (gameViewHeight / 20).toFloat()
+            for(i in newGameBoard){
+                counterJ = 0
+                for(j in i){
+                    if (j == 1){
+                        canvas.drawRect(counterJ.toFloat()*multiplierI,
+                            counterI.toFloat()*multiplierJ,
+                            (counterJ+1).toFloat()*multiplierI, (counterI+1).toFloat()*multiplierJ, paint)
+                    }
+                    counterJ += 1
+                }
+                counterI += 1
+            }
+            binding.gameSpaceView.setImageDrawable(BitmapDrawable(resources, bitmap))
+        })
+
+
+        // Button action
         binding.endButton.setOnClickListener {
         }
         binding.pauseButton.setOnClickListener {
         }
 
+        // Chronometer
         chronometer = binding.chronometer
         chronometer.format = "Time: %s"
-
-        gameModel = GameViewModel(binding.gameSpaceView, this)
         startChronometer()
         chronometer.onChronometerTickListener = OnChronometerTickListener { chronometer ->
             if (SystemClock.elapsedRealtime() - chronometer.base >= 1000) {
                 val time = SystemClock.elapsedRealtime()
+                gameModel.oneTickGame()
                 Log.d("Time check", "$time")
             }
         }
+
         return binding.root
     }
 
--- app/src/main/java/com/example/tetris/game/GameViewModel.kt
+++ app/src/main/java/com/example/tetris/game/GameViewModel.kt
@@ -9,80 +9,122 @@ import android.graphics.drawable.BitmapDrawable
 import android.util.Log
 import android.widget.Chronometer
 import android.widget.ImageView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.tetris.get_started.GameFragment
 
-class GameViewModel(imageView: ImageView, context: GameFragment) : ViewModel(){
+class GameViewModel() : ViewModel(){
 
-    var bitmap : Bitmap
-    var canvas: Canvas
-    var paint: Paint
-    var gameView = imageView
-    var gameContext = context
-    private val gameViewWidth : Int = gameView.layoutParams.width
-    private var gameViewHeight : Int = gameView.layoutParams.height
-    var gameBoard = arrayOf(
-        arrayOf(0,0,0,0,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,0,0,0,1,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,1,0,0,0,0,0),
-        arrayOf(0,0,0,0,0,0,0,0,0,0),
-        arrayOf(0,0,1,0,0,0,0,0,0,1),
-        arrayOf(0,0,0,1,0,0,0,0,0,0),
-        arrayOf(0,0,0,0,1,0,0,0,0,0),
-        arrayOf(1,1,1,1,1,1,1,1,1,1),
-    )
+//    var bitmap : Bitmap
+//    var canvas: Canvas
+//    var paint: Paint
+//    var gameView = imageView
+//    var gameContext = context
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    // Game board
+    private val _gameBoard = MutableLiveData<ArrayList<ArrayList<Int>>>()
+    val gameBoard: LiveData<ArrayList<ArrayList<Int>>>
+        get() = _gameBoard
+
+//
+//    private val gameViewWidth : Int = gameView.layoutParams.width
+//    private var gameViewHeight : Int = gameView.layoutParams.height
+//    var gameBoard = arrayOf(
+//        arrayOf(0,0,0,0,0,0,0,0,0,0),
+//        arrayOf(0,0,0,0,0,0,0,1,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,0),
+//        arrayOf(0,0,0,0,1,0,0,0,0,0),
+//        arrayOf(0,0,0,0,0,0,0,0,0,0),
+//        arrayOf(0,0,1,0,0,0,0,0,0,1),
+//        arrayOf(0,0,0,1,0,0,0,0,0,0),
+//        arrayOf(0,0,0,0,1,0,0,0,0,0),
+//        arrayOf(1,1,1,1,1,1,1,1,1,1),
+//    )
     init {
-        var gameView = imageView
-
-        bitmap = Bitmap.createBitmap(gameViewWidth, gameViewHeight, Bitmap.Config.ARGB_8888)
-        canvas = Canvas(bitmap)
-        paint = Paint()
-        paint.setColor(Color.MAGENTA)
-        paint.setStyle(Paint.Style.STROKE)
-
-        drawGameBoard()
-//        gameBoard[0][5] = 1
+        _score.value = 0
+        _gameBoard.value = arrayListOf(
+            arrayListOf(0,0,0,0,0,0,0,0,0,0),
+            arrayListOf(0,0,0,0,0,0,0,1,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,0),
+            arrayListOf(0,0,0,0,1,0,0,0,0,0),
+            arrayListOf(0,0,0,0,0,0,0,0,0,0),
+            arrayListOf(0,0,1,0,0,0,0,0,0,1),
+            arrayListOf(0,0,0,1,0,0,0,0,0,0),
+            arrayListOf(0,0,0,0,1,0,0,0,0,0),
+            arrayListOf(1,1,1,1,1,1,1,1,1,1),
+        )
+//        var gameView = imageView
+//
+//        bitmap = Bitmap.createBitmap(gameViewWidth, gameViewHeight, Bitmap.Config.ARGB_8888)
+//        canvas = Canvas(bitmap)
+//        paint = Paint()
+//        paint.setColor(Color.MAGENTA)
+//        paint.setStyle(Paint.Style.STROKE)
+//
+//        drawGameBoard()
+////        gameBoard[0][5] = 1
 
 
     }
 
-    fun drawGameBoard(){
-        var counter_i : Int = 0
-        var counter_j : Int = 0
-        val multiplier_i : Float = (gameViewWidth / 10).toFloat()
-        val multilier_j : Float = (gameViewHeight / 20).toFloat()
-        for(i in gameBoard){
-            counter_j = 0
-            for(j in i){
-                if (j == 1){
-                    canvas.drawRect(counter_j.toFloat()*multiplier_i,
-                        counter_i.toFloat()*multilier_j,
-                        (counter_j+1).toFloat()*multiplier_i, (counter_i+1).toFloat()*multilier_j, paint)
-                }
-                counter_j += 1
-            }
-            counter_i += 1
-        }
-//        gameView.background = BitmapDrawable(gameContext.resources, bitmap)
-        gameView.setImageDrawable(BitmapDrawable(gameContext.resources, bitmap))
-    }
+//    fun drawGameBoard(){
+//        var counter_i : Int = 0
+//        var counter_j : Int = 0
+//        val multiplier_i : Float = (gameViewWidth / 10).toFloat()
+//        val multilier_j : Float = (gameViewHeight / 20).toFloat()
+//        for(i in gameBoard){
+//            counter_j = 0
+//            for(j in i){
+//                if (j == 1){
+//                    canvas.drawRect(counter_j.toFloat()*multiplier_i,
+//                        counter_i.toFloat()*multilier_j,
+//                        (counter_j+1).toFloat()*multiplier_i, (counter_i+1).toFloat()*multilier_j, paint)
+//                }
+//                counter_j += 1
+//            }
+//            counter_i += 1
+//        }
+////        gameView.background = BitmapDrawable(gameContext.resources, bitmap)
+//        gameView.setImageDrawable(BitmapDrawable(gameContext.resources, bitmap))
+//    }
 
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+
+    fun oneTickGame(){
+        _score.value = (score.value)?.plus(1)
+
     }
 
 }
